b96bfe8e6b817829999290fd16594a56431fcd60
Vanshika <vanshika06065@gmail.com>
2023-02-17 22:18:56 +0530

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const cors = require('cors')
 const app= express();
+
+
 const cookieParser= require("cookie-parser")
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 
